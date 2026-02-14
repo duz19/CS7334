bf4ed8067fef4bc481d3afb81c15b64efdb03359
Md Khairul Azman <duz19@login1.ls6.tacc.utexas.edu>
2026-02-13 23:31:27 -0600

    output

---
+++ Lab1/dot_prod_correct/dot_prod.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <omp.h>
+
+float dot_prod(float* a, float* b, int N)
+{
+    float sum = 0.0;
+
+    #pragma omp parallel for reduction(+:sum)
+    for(int i = 0; i < N; i++) {
+        sum += a[i] * b[i];
+    }
+
+    return sum;
+}
+
+int main() {
+
+    // Hello from threads
+    #pragma omp parallel
+    {
+        int tid = omp_get_thread_num();
+        int nthreads = omp_get_num_threads();
+        printf("Hello from thread %d out of %d threads\n", tid, nthreads);
+    }
+
+    // Example vectors
+    float a[] = {1.0, 2.0, 3.0, 4.0};
+    float b[] = {5.0, 6.0, 7.0, 8.0};
+    int N = 4;
+
+    // Compute dot product
+    float result = dot_prod(a, b, N);
+
+    printf("\nDot Product = %f\n", result);
+
+    return 0;
+}
+
